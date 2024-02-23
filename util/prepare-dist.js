2037e4f9be59d8a2962bc085b3d1fbf22c9a3fe6
Claus Nuoskanen <claus.nuoskanen@gmail.com>
2024-02-23 06:45:21 +0200

    Fixed build process issues

--- util/prepare-dist.js
+++ util/prepare-dist.js
@@ -5,6 +5,11 @@ const packageJson = require('../package.json');
 const removeFields = ['scripts', 'devDependencies']
 const alterFields = ['main', 'types', 'files', 'exports', 'typesVersions']
 const copyFiles = ['README.md', 'LICENSE'];
+const entryPoints = {
+    'index.js': 'cogni',
+    'async.js': 'asyncCogni',
+    'memo.js': 'memo',
+}
 
 for (const field of removeFields) {
     delete packageJson[field];
@@ -18,6 +23,16 @@ fs.writeFileSync(path.join(__dirname, '..', 'dist', 'package.json'), JSON.string
 
 for (const file of copyFiles) {
     fs.copyFileSync(path.join(__dirname, '..', file), path.join(__dirname, '..', 'dist', file));
+}
+
+for (const [entry, name] of Object.entries(entryPoints)) {
+    const tpl = `const ${name} = require('./cjs/${entry}');
+module.exports = ${name}.default || ${name};
+`;
+    fs.writeFileSync(path.join(__dirname, '..', 'dist', entry), tpl);
+
+    const types = entry.replace('.js', '.d.ts');
+    fs.copyFileSync(path.join(__dirname, '..', 'dist', 'cjs', types), path.join(__dirname, '..', 'dist', types));
 }
 
 function alterField(value) {
